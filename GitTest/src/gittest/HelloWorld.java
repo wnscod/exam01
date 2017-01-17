f0344502c522f86f8f6e234fa9665decfeb0e7d8
it <it@DESKTOP-KKAQ0RK>
2017-01-17 09:12:44 +0900

    HelloWordCommit

---
+++ GitTest/src/gittest/HelloWorld.java
@@ -0,0 +1,11 @@
+package gittest;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+		//Commit and push는 로컬과 깃허브둘다 저장되나 Commit은 로컬폴더에만 저장
+	}
+
+}
