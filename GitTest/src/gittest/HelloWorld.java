2354a873b9a9a264cddbe1d426f287a93cebd1ac
it <it@DESKTOP-KKAQ0RK>
2017-01-17 09:19:26 +0900

    brachCommit

--- GitTest/src/gittest/HelloWorld.java
+++ GitTest/src/gittest/HelloWorld.java
@@ -6,7 +6,9 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld");
 		System.out.println("master");
+		System.out.println("branch");
 		//Commit and push는 로컬과 깃허브둘다 저장되나 Commit은 로컬폴더에만 저장
+		//commit을 하는이유는 백업을 시켜놓기위해서
 	}
 
 }
